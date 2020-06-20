be92881f6c1a8e438e49de6fdd37c7ea90778e92
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-06-20 17:50:47 -0300

    Initial Commit

---
+++ Spring-Java8-Gradle-MultiProject/src/main/java/aug/bueno/Application.java
@@ -0,0 +1,13 @@
+package aug.bueno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
